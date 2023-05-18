3e735fab444014c3251d0f9531cead24167cd377
angelsoempty <denatamanov1@gmail.com>
2023-05-18 19:11:41 +0300

    ітератор

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 #ітератор
 lst = [1,2,3,4,5,6,7,8,8]
 print(iter(lst))
+
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+for num in MyIterator(lst):
+    print(num)
